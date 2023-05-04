6c42393fdef955eb74654a8e9324c2f2d01b8aad
kkb <0furiousblade0@gmail.com>
2023-05-04 17:23:54 +0900

    5/4 과제 제출

--- Quest08/skeleton/server.mjs
+++ Quest08/skeleton/server.mjs
@@ -1,8 +1,58 @@
 import http from 'http';
+import url from 'url';
+import request from 'request';
 
-const server = http.createServer((req, res) => {
-    /* TODO: 각각의 URL들을 어떻게 처리하면 좋을까요? */
-    res.end();
+
+const server = http.createServer((req, res) => { // 서버생성
+
+
+    server.listen(8000);
+
 });
 
-server.listen(8000);
+
+
+
+
+/*
+
+
+
+*/ 
+    
+    /*
+
+    var geturl = 'http://localhost:8000/foo' + bar
+    request.get({
+        url: geturl
+    });
+
+    var posturl = 'http://localhost:8000/foo' + bar
+    request.post({
+        url: posturl
+    });
+*/
+
+/* 
+
+    let {url, headers, method} = req
+
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+    res.write('<p>Hello World!</p>');
+    res.end();
+
+    const bar = "hi";
+    
+    if (req.url == '/foo'){
+        res.write("a")
+
+    } else if(method === 'GET') {
+        res.write("GET");
+        res.end();
+
+    } else if(method === 'POST'){
+        res.write("POST");
+        res.end();
+        }
+
+*/
